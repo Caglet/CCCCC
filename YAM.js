73b2bbc0a1769a5c20dec4c8797ac137efe29cf1
Caglet <FrewgameTv@gmail.com>
2025-02-13 14:54:54 +0700

    Delete YAM.js

--- YAM.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('proposalButton').addEventListener('click', function () {
-        window.location.href = 'Marry.html';
-        
-    });
-});
